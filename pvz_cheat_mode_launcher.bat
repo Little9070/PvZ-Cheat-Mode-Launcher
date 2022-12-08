81144d15a1069e47ce079857753fc136075f3026
Little9070 <104382303+Little9070@users.noreply.github.com>
2022-12-07 23:03:33 -0600

    Add files via upload

--- pvz_cheat_mode_launcher.bat
+++ pvz_cheat_mode_launcher.bat
@@ -31,7 +31,12 @@ start PlantsVsZombies.exe -tod
 exit /b
 
 :help
-echo
+echo Place this in the installation directory of PvZ, so it should be next to main.pak and your PvZ executable file.
+echo If you get a file not found error, make sure you're pressing the right version in the batch file, if that error continues make sure it's named BloomAndDoom.exe or PlantsVsZombies.exe (capitalization doesn't matter.)
+echo If you get "invalid command line parameter: -tod" that means you aren't using a beta version.
+echo If you have any bugs/requests, send me a DM on Discord at "Little9070#3932". (Caps sensitive)
+echo GUI coming soon.
+pause
 exit /b
 
 :term
